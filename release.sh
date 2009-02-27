94e856889193bc57151267f7a1e1d1c30773b5d4
Kris Steinhoff <steinhof@umich.edu>
2009-02-27 15:19:07 -0500

    moved bash config file

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files="bashrc_common.sh inputrc screenrc vimrc gitconfig gitexcludes"
+files="bashrc_common inputrc screenrc vimrc gitconfig gitexcludes"
 
 if [ "x${1}" == "x-h" ]; then
     echo "usage: `basename ${0}` [ host ]"
