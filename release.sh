c7f98ec3af7cea51706acbfbe0a6c1faecf6d753
Kris Steinhoff <steinhof@umich.edu>
2009-04-22 11:02:00 -0400

    can release specific files to home with host specified as "-"

--- release.sh
+++ release.sh
@@ -3,7 +3,10 @@
 if [ "x${1}" == "x-h" ]; then
     echo "usage: `basename ${0}` [ host ] [ file ]"
     exit 1
-elif [ "x${1}" == "x" ]; then
+elif [ "x${1}" == "x" -o "${1}" == "-" ]; then
+    if [ "${1}" == "-" ]; then
+        shift;
+    fi
     cmd=cp
     dest="${HOME}/"
 else
