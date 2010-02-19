b290858a9375fe194a777c5e82eda8f2ce7650e6
Kris Steinhoff <steinhof@umich.edu>
2010-02-19 13:57:15 -0500

    fixed release.sh script

--- release.sh
+++ release.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-if [ "x${1}" == "x-h" ]; then
+if [ "x${1}" = "x-h" ]; then
     echo "usage: `basename ${0}` [ host ] [ file ]"
     exit 1
-elif [ "x${1}" == "x" -o "${1}" == "-" ]; then
-    if [ "${1}" == "-" ]; then
-        shift;
-    fi
+elif [ "x${1}" = "x" ]; then
+    cmd=cp
+    dest="${HOME}/"
+elif [ "x${1}" = "x-" ]; then
+    shift;
     cmd=cp
     dest="${HOME}/"
 else
