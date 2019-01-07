4a0830b5378aab1b8ecb2986a1f0980ec0323578
Kris Steinhoff <steinhof@umich.edu>
2019-01-07 15:40:57 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
-mv .bashrc .bashrc.bak
+mv .zshrc .zshrc.bak
+mv.bashrc .bashrc.bak
 mv .bash_profile .bash_profile.bak
+
 git init --bare $HOME/.dotfiles
 alias dotfiles='$(which git) -c status.showUntrackedFiles=no --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles remote add origin https://github.com/ksofa2/dotfiles.git
 dotfiles pull origin master
 dotfiles branch --set-upstream-to=origin/master master
-dotfiles submodule update --init  # new version of git, you can add --depth=100 to speed things up
