ce2f87d7415d708f93ccae6e79c11d8f5a5d5d98
Kris Steinhoff <steinhof@umich.edu>
2019-01-07 15:42:56 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ mv .bash_profile .bash_profile.bak
 
 git init --bare $HOME/.dotfiles
 alias dotfiles='$(which git) -c status.showUntrackedFiles=no --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
 dotfiles remote add origin https://github.com/ksofa2/dotfiles.git
+dotfiles fetch
 dotfiles pull origin master
 dotfiles branch --set-upstream-to=origin/master master
