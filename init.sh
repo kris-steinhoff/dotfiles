66b24a97d4e19ad23ffb321a4e2dd1ee39cbc569
Kris Steinhoff <steinhof@umich.edu>
2019-01-12 15:48:10 -0500

    Improve init script

--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
-mv .zshrc .zshrc.bak
-mv .bashrc .bashrc.bak
-mv .bash_profile .bash_profile.bak
+#!/bin/bash
 
-git init --bare $HOME/.dotfiles
-alias dotfiles='$(which git) -c status.showUntrackedFiles=no --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+DOTFILES_INSTALL_DIR=$(pwd)
+
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "${DOTFILES_INSTALL_DIR}/.oh-my-zsh"
+git clone --depth=1 https://github.com/Bash-it/bash-it.git "${DOTFILES_INSTALL_DIR}/.bash_it"
+
+mv -v .zshrc .zshrc.bak
+mv -v .bashrc .bashrc.bak
+mv -v .bash_profile .bash_profile.bak
+
+git init --bare "${DOTFILES_INSTALL_DIR}/.dotfiles"
+alias dotfiles='$(which git) -c status.showUntrackedFiles=no --git-dir=$DOTFILES_INSTALL_DIR/.dotfiles/ --work-tree=$DOTFILES_INSTALL_DIR'
 
 dotfiles remote add origin https://github.com/ksofa2/dotfiles.git
 dotfiles fetch
