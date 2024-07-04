495934e531d468342c63322a31a7848d7a824335
Kris Steinhoff <ksteinhoff@gmail.com>
2024-07-04 11:16:11 -0400

    Prevent bad exit status if no .local-post.sh

--- .common-post.sh
+++ .common-post.sh
@@ -6,4 +6,4 @@ alias dco="docker-compose"
 export EDITOR="${OVERRIDE_EDITOR-'vim'}"
 
 test -f "${HOME}/.local.sh" && echo "WARNING: ${HOME}/.local.sh is deprecated, move it to ${HOME}/.local-post.sh" && . "${HOME}/.local.sh"
-test -f "${HOME}/.local-post.sh" && . "${HOME}/.local-post.sh"
+test -f "${HOME}/.local-post.sh" && . "${HOME}/.local-post.sh" || true
