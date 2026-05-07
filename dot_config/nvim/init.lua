fa3e26542c4717a6f102c1c7a41434f6c2db735b
Kris Steinhoff <kris@ofa2.com>
2026-05-06 20:28:34 -0400

    Replace LazyVim config with vanilla lazy.nvim baseline

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,41 @@
-require("config.lazy")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local out = vim.fn.system({
+    "git", "clone", "--filter=blob:none", "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git", lazypath,
+  })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.showcmd = true
+vim.opt.showmode = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Markdown: disable concealment so syntax is visible as typed
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
+require("lazy").setup({}, {
+  checker = { enabled = false },
+})
