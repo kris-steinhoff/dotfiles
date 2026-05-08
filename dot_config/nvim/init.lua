1b3624ed30dacc58f3aec6616b30a03a261e4b59
Kris Steinhoff <kris@ofa2.com>
2026-05-07 22:29:25 -0400

    Replace toggleterm with snacks; add claudecode.nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -36,14 +36,54 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.keymap.set("n", "<leader>th", function()
+  Snacks.terminal.toggle(nil, { win = { position = "bottom" } })
+end, { desc = "Toggle horizontal terminal" })
+vim.keymap.set({ "n", "t" }, "<C-\\>", function()
+  Snacks.terminal.toggle(nil, { win = { position = "float", border = "rounded" } })
+end, { desc = "Toggle floating terminal" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h")
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j")
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k")
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l")
+
 require("lazy").setup({
   {
-    "akinsho/toggleterm.nvim",
-    version = "*",
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
     opts = {
-      open_mapping = [[<c-\>]],
-      direction = "float",
-      float_opts = { border = "curved" },
+      bigfile = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      picker = { enabled = true },
+      terminal = {},
+    },
+  },
+  {
+    "coder/claudecode.nvim",
+    config = true,
+    keys = {
+      { "<leader>a", nil, desc = "AI/Claude Code" },
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+      { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+      { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+      { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select model" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send selection" },
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
     },
   },
 }, {
